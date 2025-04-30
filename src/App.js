a261d1271876393d4a239f0a66d00e2b8e3856f0
Mulero Alamou <mulero.alamou@gmail.com>
2025-04-30 16:24:31 -0700

    Fixed useMemo issue that caused components to not render

--- src/App.js
+++ src/App.js
@@ -1,58 +1,70 @@
+import React, { useRef, useEffect, useMemo, useCallback } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import {useRef, useEffect, useMemo} from 'react';
-import {About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from './components';
 
 function App() {
   const heroRef = useRef(null);
 
-  const updateMousePosition = (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
+  const updateMousePosition = useCallback((e) => {
+    if (!heroRef.current) return;
+    const { clientX: x, clientY: y } = e;
     heroRef.current.style.setProperty('--x', `${x}px`);
     heroRef.current.style.setProperty('--y', `${y}px`);
-  }
+  }, []);
 
-  const throttle = (func, delay) => {
-    let waiting = false;
-
-    return (...args) => {
-      if(waiting === false){
-        func(...args);
-        waiting = setTimeout(() => {
-          waiting = false;
-        },delay)
+  function throttle(fn, delay) {
+    let timer = null;
+    return function (...args) {
+      if (timer === null) {
+        fn(...args);
+        timer = setTimeout(() => {
+          timer = null;
+        }, delay);
       }
-    }
+    };
   }
 
-  const throttledUpdateMousePosition = useMemo(throttle(updateMousePosition,100),[]);
+  const throttledUpdateMousePosition = useMemo(
+    () => throttle(updateMousePosition, 100),
+    [updateMousePosition]
+  );
 
   useEffect(() => {
-    if(!heroRef.current) return;
-
     window.addEventListener('mousemove', throttledUpdateMousePosition);
 
-    return () => window.removeEventListener('mousemove', throttledUpdateMousePosition);
-  }, []);
+    updateMousePosition({
+      clientX: window.innerWidth / 2,
+      clientY: window.innerHeight / 2,
+    });
+
+    return () =>
+      window.removeEventListener('mousemove', throttledUpdateMousePosition);
+  }, [throttledUpdateMousePosition, updateMousePosition]);
 
   return (
     <BrowserRouter>
       <div ref={heroRef} className="hero relative z-0 bg-primary">
-        <div>
-          <Navbar />
-          <Hero/>
-        </div>
-        <About/>
-        <Experience/>
-        <Tech/>
-        <Works/>
-        <div className='relative z-0'>
-          <Contact/>
-          <StarsCanvas numStars={4000}/>
+        <Navbar />
+        <Hero />
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas numStars={4000} />
         </div>
       </div>
     </BrowserRouter>
-
   );
 }
 
